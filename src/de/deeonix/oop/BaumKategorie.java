61396e33eda0f23ce5e3753d7d8b0df8a1d33bf8
Deeonix <deeonix@github.com>
2019-06-01 01:20:39 +0200

    Comment BaumKategorie

--- src/de/deeonix/oop/BaumKategorie.java
+++ src/de/deeonix/oop/BaumKategorie.java
@@ -1,5 +1,9 @@
 package de.deeonix.oop;
 
 public enum BaumKategorie {
+    /*
+    * Enum steht für Enumeration, also für Aufzählung.
+    * Hier werden die verschiedenen Baumarten aufgezählt.
+    * */
     BIRKE, EICHE, ESCHE, NILS, TANNE, AHORN, FICHTE, BUCHE
 }
